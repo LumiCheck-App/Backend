80b1897de9c7f55bcb86afaa1c3d1dc2aa31f7fa
Rodrigo Graça <rodrigomgraca@ua.pt>
2025-05-30 14:57:17 +0100

    CHANGE MAIN.PY

--- app/main.py
+++ app/main.py
@@ -55,4 +55,7 @@ app.include_router(screen_time_router, prefix="/screentime", tags=["Screen Time"
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(sio_app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(sio_app, host="0.0.0.0", port=port)
+
