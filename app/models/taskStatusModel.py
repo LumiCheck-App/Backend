66122abbe88eae303e478eba36c465d71adb8238
Rodrigo Graça <rodrigomgraca@ua.pt>
2025-06-21 01:55:41 +0100

    task statussss

--- app/models/taskStatusModel.py
+++ app/models/taskStatusModel.py
@@ -3,7 +3,8 @@ from config import Base
 
 class UserTaskStatus(Base):
     __tablename__ = "task_status"
-    id_user = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"), primary_key=True)
-    id_task = Column(Integer, ForeignKey("task.id", ondelete="CASCADE"), primary_key=True)
+    id_user = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    id_task = Column(Integer, ForeignKey("task.id", ondelete="CASCADE"))
     done = Column(Boolean, default=False)
     completed_at = Column(DateTime, nullable=True)
+    id = Column(Integer, primary_key=True, index=True)
